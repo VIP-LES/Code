81730c4e26d2f12641eab4e89e25209d1880b14c
asantiago33 <asantiago33@gatech.edu>
2016-10-06 23:23:22 +0000

    u

--- dataReader.py
+++ dataReader.py
@@ -3,11 +3,10 @@ import serial;
 
 
 def writeToCSV(filename):
-    while (True) {
-        newfile.write(ser.readline);
-    }
+    while (True):
+        newfile.write(ser.readline())
 
 if __name__ == "__main__":
-    ser = serial.Serial("") #serial Port here
-    file newfile = open("newfile.csv", "w");
+    ser = serial.Serial("/dev/ttyACM0" , 9600) #serial Port here
+    newfile = open("newfile.csv", "w")
     writeToCSV(newfile)
