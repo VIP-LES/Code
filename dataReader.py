115677ae30d9fbbc21d9d1c25afe301c287985fa
Arihan Shah <arihan.shah@gmail.com>
2016-10-06 18:42:23 -0400

    Add files via upload

---
+++ dataReader.py
@@ -0,0 +1,13 @@
+import serial;
+
+
+
+def writeToCSV(filename):
+    while (True) {
+        newfile.write(ser.readline);
+    }
+
+if __name__ == "__main__":
+    ser = serial.Serial("") #serial Port here
+    file newfile = open("newfile.csv", "w");
+    writeToCSV(newfile)
