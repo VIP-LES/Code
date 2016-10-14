20ab2ce6e7e30cf5d0a11fc0dc62bf384f136e40
asantiago33 <asantiago33@gatech.edu>
2016-10-14 21:57:44 +0000

    datareder update

--- dataReader.py
+++ dataReader.py
@@ -4,9 +4,12 @@ import serial;
 
 def writeToCSV(filename):
     while (True):
-        newfile.write(ser.readline())
+        line = ser.readline()
+        print(line)
+        newfile.write(line)
 
 if __name__ == "__main__":
     ser = serial.Serial("/dev/ttyACM0" , 9600) #serial Port here
     newfile = open("newfile.csv", "w")
+    newfile.write("Sensor_1, Sensor_2")
     writeToCSV(newfile)
