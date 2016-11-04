ed6643149035eef4f4c3aa086034d3a201cbb908
Arihan Shah <arihan.shah@gmail.com>
2016-11-04 16:58:19 -0400

    Add files via upload

--- newdatareader.py
+++ newdatareader.py
@@ -23,25 +23,25 @@ GPIO.setup(36, GPIO.IN)
 while (GPIO.imput(32) == 1):
     if (GPIO.input(7) == 1):
         print('0')
-    if else (GPIO.input(11) == 1):
+    elif (GPIO.input(11) == 1):
         print('1')
-    if else (GPIO.input(13) == 1):
+    elif (GPIO.input(13) == 1):
         print('2')
-    if else (GPIO.input(15) == 1):
+    elif (GPIO.input(15) == 1):
         print('3')
-    if else (GPIO.input(29) == 1):
+    elif (GPIO.input(29) == 1):
         print('4')
-    if else (GPIO.input(31) == 1):
+    elif (GPIO.input(31) == 1):
         print('5')
-    if else (GPIO.input(33) == 1):
+    elif (GPIO.input(33) == 1):
         print('6')
-    if else (GPIO.input(35) == 1):
+    elif (GPIO.input(35) == 1):
         print('7')
-    if else (GPIO.input(37) == 1):
+    elif (GPIO.input(37) == 1):
         print('8')
-    if else (GPIO.input(40) == 1):
+    elif (GPIO.input(40) == 1):
         print('9')
-    if else (GPIO.input(38) == 1):
+    elif (GPIO.input(38) == 1):
         print(',')
-    if else (GPIO.input(36) == 1):
+    elif (GPIO.input(36) == 1):
         print('\n')
