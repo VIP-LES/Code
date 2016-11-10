1745eceea288a9ca578b599b5d5631ed7e81d628
Arihan Shah <arihan.shah@gmail.com>
2016-11-10 18:33:51 -0500

    Update dataReader.py

--- dataReader.py
+++ dataReader.py
@@ -1,15 +1,22 @@
 import serial;
+import datetime;
+import time;
+
 
 
 
 def writeToCSV(filename):
     while (True):
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         line = ser.readline()
+        line = (st + " , ") + line
         print(line)
         newfile.write(line)
 
 if __name__ == "__main__":
-    ser = serial.Serial("/dev/ttyACM0" , 9600) #serial Port here
+    ser = serial.Serial("/dev/ttyACM0" , 250000) #serial Port here
     newfile = open("newfile.csv", "w")
-    newfile.write("Sensor_1, Sensor_2")
+    newfile.write("Sensor_1")
+    
     writeToCSV(newfile)
