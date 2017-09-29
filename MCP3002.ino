f9553f094908f9522e6248e15618a2918a5af317
Matthew Pruett <matthewtpruett@yahoo.com>
2017-09-29 13:08:12 -0400

    Updated to manual change pin outputs
    The Arduino SPI library does not work properly with the MCP3002, so changes were made to manually control the SPI pin outputs.

--- MCP3002.ino
+++ MCP3002.ino
@@ -2,44 +2,91 @@
 // Code outputs an analog signal with PWM to the
 // ADC which outputs a digital signal to the 
 // Arduino using SPI.
+//
 // Pins:
-// MCP3002 <-> Arduino
+// MCP3002  | Arduino Pin  |  Name
 //     Vdd  |  5V
-//     CH0  |  D3
-//     CLK  |  D13
-//     Din  |  D11
-//     Dout |  D12
-//     CS'  |  D10
+//     Gnd  |  Gnd
+//     CH0  |  3
+//     CLK  |  13             SCK
+//     Din  |  11             MOSI
+//     Dout |  12             MISO
+//     CS'  |  10             SS
 
-#include <SPI.h>
-
-int data = 0;
+// Global variables for pin numbers
+const int clk = 13;
+const int din = 11;
+const int dout = 12;
+const int cs = 10;
 
 void setup() {
-  // Begin serial for data output
+
+  // Serial setup
   Serial.begin(9600);
 
-  // Set pin 3 to OUTPUT for PWM
-  pinMode(3, OUTPUT);
+  // Set pin modes
+  pinMode(clk, OUTPUT);
+  pinMode(din, OUTPUT);
+  pinMode(dout, INPUT);
+  pinMode(cs, OUTPUT);
 
-  // SPI setup
-  SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
+  // Set beginning pin outputs
+  digitalWrite(clk, LOW);
+  digitalWrite(din, LOW);
+  digitalWrite(cs, HIGH);
+
+  // Generate 50% duty cycle PWM for testing only
+  pinMode(3, OUTPUT);
+  analogWrite(3, 127);
+  
 
 }
 
-void loop() {
-  // Output 50% duty cycle PWM signal on pin D3
-  analogWrite(3, 127);
+int readADC(byte settings) {
+  // Create variable for return value
+  int data = 0;
 
-  // Send data to MCP3002 for setup and receive digital signal
+  digitalWrite(cs, LOW);
+
+  // Send setup data to ADC
+  for (int i = 3; i >= 0; i++) {
+    //digitalWrite(din, (settings & (1 << i)) ? HIGH : LOW);
+    digitalWrite(din, settings & (1 << i));
+    digitalWrite(clk, HIGH);
+    digitalWrite(clk, LOW);
+  }
+
+  // Ignore null bit
+  digitalWrite(clk, HIGH);
+  digitalWrite(clk, LOW);
+
+  // Read 10 bits of data from ADC
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(clk, HIGH);
+    data = (data << 1) | digitalRead(dout);
+    digitalWrite(clk, LOW);
+  }
+
+  digitalWrite(cs, HIGH);
+
+  return data;
+  
+}
+
+void loop() {
+
+  // create data variable to store data from ADC
+  int data = 0;
+
+  // Send data to MCP3002 for setup and store digital signal in data variable
   // First bit high indicates start
   // Second bit high indicates single ended mode
   // Third bit low indicates channel 0
   // Fourth bit high indicates MSB first
-  data = SPI.transfer(0xD);
+  data = readADC(0xD);
 
-  // Print data
-  Serial.print(data);
-  
+  // Print data and wait 1 second
+  Serial.println(data);
+  delay(1000);
 
 }
